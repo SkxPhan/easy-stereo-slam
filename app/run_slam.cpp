e2f00006e2106995a2dfb3695bed5435d9606e94
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2025-04-20 20:36:32 +0900

    Cleaning

--- app/run_slam.cpp
+++ app/run_slam.cpp
@@ -1,15 +1,13 @@
+#include <cassert>
 #include <gflags/gflags.h>
 #include <iostream>
 #include <memory>
-#include <cassert>
-#include <filesystem>
 
 #include "visual_odometry.hpp"
 
 DEFINE_string(config_file, "./config/default.yaml", "config file path");
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
 
   std::cout << "Loading config file: " << FLAGS_config_file << "\n";
